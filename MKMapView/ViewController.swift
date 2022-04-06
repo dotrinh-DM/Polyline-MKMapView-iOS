26587303cbd343c2b3bd1a28c180d0fa64683938
Do Trinh <contact@dotrinh.com>
2022-04-06 14:41:13 +0900

    auto center, auto follow

--- MKMapView/ViewController.swift
+++ MKMapView/ViewController.swift
@@ -12,10 +12,10 @@ import CoreLocation
 class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     @IBOutlet weak var customMkMapV: MKMapView!
+    @IBOutlet weak var autoCenter: UISwitch!
     
     var locationManager = CLLocationManager()
     var coordArray:[CLLocationCoordinate2D] = []
-    var centerAnchor:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +34,11 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         customMkMapV.center = view.center
         
         // adding co-ordinates for poly line (added static, we can make it dyanamic)
-        let coords1 = CLLocationCoordinate2D(latitude: 35.555139, longitude: 139.722241)
-        let coords2 = CLLocationCoordinate2D(latitude: 35.555009941986874, longitude: 139.72272022691322)
-        let coords3 = CLLocationCoordinate2D(latitude: 35.55596118732464, longitude: 139.7230868058718)
-        let coords4 = CLLocationCoordinate2D(latitude: 35.55651062646226, longitude: 139.7212121315655)
-        let coords5 = CLLocationCoordinate2D(latitude: 35.55573046119005, longitude: 139.72054398577677)
+//        let coords1 = CLLocationCoordinate2D(latitude: 35.555139, longitude: 139.722241)
+//        let coords2 = CLLocationCoordinate2D(latitude: 35.555009941986874, longitude: 139.72272022691322)
+//        let coords3 = CLLocationCoordinate2D(latitude: 35.55596118732464, longitude: 139.7230868058718)
+//        let coords4 = CLLocationCoordinate2D(latitude: 35.55651062646226, longitude: 139.7212121315655)
+//        let coords5 = CLLocationCoordinate2D(latitude: 35.55573046119005, longitude: 139.72054398577677)
         
         // updating current location method
         if CLLocationManager.locationServicesEnabled() {
@@ -53,10 +53,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         let currentPosition = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
         coordArray.append(currentPosition)
         let testline = MKPolyline(coordinates: coordArray, count: coordArray.count)
-        if(centerAnchor == 0){
+        if(autoCenter.isOn){
             let region = MKCoordinateRegion(center: currentPosition, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             customMkMapV.setRegion(region, animated: true)
-            centerAnchor+=1
         }
         customMkMapV.addOverlay(testline)
     }
